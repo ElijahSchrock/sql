a55eb1871f998261732226f6d0e1be4c4e6d5ce4
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2022-04-20 17:29:49 -0700

    sql injection

---
+++ app.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const sqlite3 = require("sqlite3").verbose();
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT
+
+const app = express();
+app.use(express.static("."));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const db = new sqlite3.Database(":memory:");
+db.serialize(function () {
+  db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
+  db.run(
+    "INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')"
+  );
+});
+
+app.get("/", function (req, res) {
+  res.sendFile("index.html");
+});
+
+app.post("/login", function (req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  var query =
+    "SELECT title FROM user where username = '" +
+    username +
+    "' and password = '" +
+    password +
+    "'";
+
+  db.get(query, function (err, row) {
+    if (err) {
+      console.log("ERROR", err);
+      res.redirect("/index.html#error");
+    } else if (!row) {
+      res.redirect("/index.html#unauthorized");
+    } else {
+      res.send(
+        "Hello <b>" +
+          row.title +
+          '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>'
+      );
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
